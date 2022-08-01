6d269af6da2ae193ccc2269db29a739447b132ba
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-01 20:40:05 -0300

    feat: add links for routes login and register

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,12 +4,18 @@ import styles from './Navbar.module.css';
 function Navbar() {
   return (
     <nav className={ styles.navbar }>
-      < NavLink to='/' className={ styles.brand }>
-        Mini <span>Blog</span>
+      < NavLink to='/'>
+        Mini <span className={ styles.brand }>Blog</span>
       </ NavLink>
       <ul className={ styles.links_list }>
         <li>
           < NavLink to='/' className={ ({ isActive }) =>  isActive ? styles.active : undefined  }>Home</ NavLink>
+        </li>
+        <li>
+          < NavLink to='/login' className={ ({ isActive }) =>  isActive ? styles.active : undefined  }>Login</ NavLink>
+        </li>
+        <li>
+          < NavLink to='/register' className={ ({ isActive }) =>  isActive ? styles.active : undefined  }>Cadastrar</ NavLink>
         </li>
         <li>
           < NavLink to='/about' className={ ({ isActive }) =>  isActive ? styles.active : undefined  }>About</ NavLink>
