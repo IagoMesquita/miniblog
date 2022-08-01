b5306c76b158b9f227ed7eed4dce3c4e9d8febdb
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-01 20:39:43 -0300

    feat: add routes login and register

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import Footer from './components/Footer';
 
 // pages
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import About from './pages/About';
+
 import NotFound from './pages/NotFound';
 
 
@@ -18,6 +21,8 @@ function App() {
       <div class="container">
         <Routes>
           <Route path='/' element={ <Home /> }/>
+          <Route path='/login' element={ <Login /> }/>
+          <Route path='/register' element={ <Register /> }/>
           <Route path='/about' element={ <About /> }/>
           <Route path='*' element={ <NotFound/> } />
         </Routes>
