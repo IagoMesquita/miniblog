1370cf16e5ab21f1620c3b3f068bc75287797086
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-15 18:19:35 -0300

    feat: page CreatePost

---
+++ src/pages/CreatePost/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './CreatePost.module.css';
+
+function CreatePost() {
+  return (
+    <div className={ styles.container }>CreatePost</div>
+  )
+}
+
+export default CreatePost;
