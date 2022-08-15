61fc7f3de30d1e7e364844703295126b7190e49b
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-15 19:46:16 -0300

    feat: finish about

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,9 +1,16 @@
-// CSS
-// import styles from './About.modules.css';
+import { Link } from 'react-router-dom';
+
+import styles from './About.module.css';
 
 function About() {
   return (
-    <h1>About</h1>
+    <div className={ styles.about }>
+      <h2>Sobre o Mini <span>Blog</span></h2>
+      <p>Este projeto consiste em um blog, feito com React no front-end e 
+        Firebase para o back-end
+      </p>
+      <Link to='/posts/create' className='btn'>Criar Post</Link>
+    </div>
   )
 }
 
