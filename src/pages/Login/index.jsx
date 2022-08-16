614a22b5ea7cd8c9565247f083f8aa657fba4e3f
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-15 21:47:33 -0300

    feat: structuring login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,64 @@
+import { useState } from 'react';
+
+import { useAuthentication } from '../../hooks/useAuthentication';
+
 import styles from './Login.module.css';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const { loading, error: authError,  } = useAuthentication();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setError('');
+
+    const user = {
+      email,
+      password
+    };
+
+
+  };
+
   return (
-    <h1>Login</h1>
+    <div className={ styles.login }>
+      <h1>Entrar</h1>
+      <p>Faça o login para poder utilizar o sistema</p>
+      <form onSubmit={ handleSubmit }>
+        <label>
+          <span>Email:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="Seu email"
+            onChange={ (e) => setEmail(e.target.value) }
+            value={ email }
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Digite sua senha"
+            onChange={ (e) => setPassword(e.target.value) }
+            value={ password }
+          />
+        </label>
+        {
+          !loading && <button className='btn'>Entrar</button>
+        }
+        {
+          loading && <button disabled="true" className='btn'>Aguarde...</button>
+        }
+        { error && <p className='error'>{error}</p> }
+      </form>
+    </div>
   )
 }
