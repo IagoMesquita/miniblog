13c8cbcb99009b951eedd95790948c3843e1fac0
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-19 19:37:23 -0300

    feat: structuring dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-
 import styles from './Dashboard.module.css'
 
+import { useState } from 'react';
+
+import { Link } from 'react-router-dom';
+
+// hooks
+import { useAuthValue } from '../../context/AuthContext';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+
 function Dashboard() {
+  
+  const { user } = useAuthValue();
+  const uid = user.id;
+  // const  = useFetchDocuments("posts");
+
+  const [posts, setPosts] = useState([]);
+
   return (
-    <div className={ styles.container }>Dashboard</div>
+    <div>
+     <h2>Dashboard</h2>
+     <p>Gerencie os seus posts</p>
+     { posts && posts.length === 0 ? (
+      <div className={ styles.noposts }>
+        <p>Não foram encontrados posts</p>
+        <Link to="/posts/create" className='btn'>Criar primeiro post</Link>
+      </div>
+     ) : (
+      <div>Algum Poste</div>
+     )}
+    </div>
   )
 }
 
