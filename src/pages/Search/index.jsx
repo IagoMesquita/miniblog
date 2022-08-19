ac2d1679dc28a62fd554c2be3e1094caa81d531a
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-19 17:35:53 -0300

    feat: render search post

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,19 +1,35 @@
+import styles from './Search.module.css';
+
+import { Link } from 'react-router-dom';
+
 // hooks
 import { useFetchDocuments } from '../../hooks/useFetchDocuments';
 import { useQuery } from '../../hooks/useQuery';
 
+// components
+import PostDetail from '../../components/PostDetail';
+
 function Search() {
-  const query = useQuery();
   // O metodo "get" veio do estânciamento do "URLSearchParams"
   // Onde "q" vem da queryStrig "/search?q=value"
+  const query = useQuery();
   const search = query.get("q")
 
+  const { documents: posts } = useFetchDocuments('posts', search);
   return (
-    <div>
+    <div className={ styles.search_container }>
       <h2>Search</h2>
-      <p>{search}</p>
+      <div>
+        { posts && posts.length === 0 && (
+          <div className={ styles.noposts }>
+            <p>Não foram encontrados posts a partir da sua busca...</p>
+            <Link to="/" className="btn btn-dark">Voltar</Link>
+          </div>
+        )}
+        { posts && posts.map((post) => <PostDetail key={ post.id } post={ post }/>) }
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Search
