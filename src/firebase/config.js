5206a15b6874ad38dfa4eba9461329cfa83e5321
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-10-19 16:25:34 -0300

    chore: envairoment

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBg4H1deT6utJxG5W3x85wXgiox6FN1vjc",
-  authDomain: "miniblog-c6b06.firebaseapp.com",
-  projectId: "miniblog-c6b06",
-  storageBucket: "miniblog-c6b06.appspot.com",
-  messagingSenderId: "893046204969",
-  appId: "1:893046204969:web:df6ae9788b3d89add30a38"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.APPID
 };
 
 // Initialize Firebase
